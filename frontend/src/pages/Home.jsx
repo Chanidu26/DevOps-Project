e871b57925b62576c0febd0f07c304c4730659c3
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2025-01-04 23:53:33 +0530

    test cicd

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
-
 const StudentManagement = () => {
   const [students, setStudents] = useState([]);
   const [formData, setFormData] = useState({
