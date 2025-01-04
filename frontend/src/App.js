ac5ce99f434418a963d9077468f08455ba754e94
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2025-01-05 00:43:36 +0530

    remove unwanted dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 
 const App = () => {
