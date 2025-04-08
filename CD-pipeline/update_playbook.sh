68b3624f4fe4d12eb853cfdff174d6341c14622e
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2025-04-08 06:48:47 +0530

    simplify update playbook script

--- CD-pipeline/update_playbook.sh
+++ CD-pipeline/update_playbook.sh
@@ -10,4 +10,4 @@ SERVER_IP=$(terraform output -raw instance_public_ip)
 cd ../ansible || exit
 
 # Replace the IP address in the playbook
-sed -i -E "s|REACT_APP_API_BASE_URL: \"http://[^\"]+:8000\"|REACT_APP_API_BASE_URL: \"http://$SERVER_IP:8000\"|g" ansible/playbook-webapp.yml
+sed -i -E "s|REACT_APP_API_BASE_URL: \"http://[^\"]+:8000\"|REACT_APP_API_BASE_URL: \"http://$SERVER_IP:8000\"|g" playbook-webapp.yml
